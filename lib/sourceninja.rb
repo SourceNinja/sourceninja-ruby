2c697833e2edbbbf0a278a8e62929fe10e69e2b5
Jeremiah Lee <jeremiah@sourceninja.com>
2012-03-13 10:33:16 -0700

    Removing logging lines because Rails hasn't started by this point in the loading of the gem. Fixing the URL and the gem name.

--- lib/sourceninja.rb
+++ lib/sourceninja.rb
@@ -1,11 +1,4 @@
-Rails.logger.debug "Sourceninja: loading..."
-
 require "sourceninja/version"
 
-Rails.logger.debug "Sourceninja: version successfully loaded."
-
-Rails.logger.debug "Sourceninja: loading main code if in Rails"
-Rails.logger.debug "Sourceninja: in Rails" if defined? Rails
 # only allow this to be run under rails for the time being.
 require "sourceninja/sourceninja.rb" if defined? Rails
-Rails.logger.debug "Sourceninja: attempted to load code" if defined? Rails
