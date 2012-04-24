fa4295ac5daa5d65fab0a698f6c66a5048fd87e6
Jeremiah Lee <jeremiah@sourceninja.com>
2012-04-24 14:34:10 -0700

    only use the Rails logger if in a Rails environment.

--- lib/sourceninja/sourceninja.rb
+++ lib/sourceninja/sourceninja.rb
@@ -12,7 +12,10 @@ module Sourceninja
     dep_list = {}
     Bundler.environment.dependencies.to_a.map{|b| b.to_s}.each do |dep|
       unless dep =~ %r{^\s*(\S+)}
-        Rails.logger.info "Sourceninja: Could not find the package name for #{dep.to_s}"
+        if defined? Rails
+          Rails.logger.info "Sourceninja: Could not find the package name for #{dep.to_s}"
+        end
+
         next
       end
 
@@ -23,14 +26,21 @@ module Sourceninja
     spec_hash = Bundler.environment.specs.to_hash
     spec_hash.keys.each do |key|
       unless %r{Gem::Specification name=#{key} version=([\d.]+)} =~ spec_hash[key][0].to_s
-        Rails.logger.info "Sourceninja: Could not parse information for gem #{key}: #{spec_hash[key]}"
+        if defined? Rails
+          Rails.logger.info "Sourceninja: Could not parse information for gem #{key}: #{spec_hash[key]}"
+        else
+          $stderr.puts "Sourceninja: Could not parse information for gem #{key}: #{spec_hash[key]}"
+        end
         next
       end
       package_data << { :package_name => key, :package_version => $1, :direct_requirement => (dep_list[key] || false) }
     end
 
     if package_data.empty?
-      Rails.logger.info "Sourceninja: Did not successfully parse any packages, will not attempt to upload information"
+      if defined? Rails
+        Rails.logger.info "Sourceninja: Did not successfully parse any packages, will not attempt to upload information"
+      end
+
       return
     end
 
@@ -38,28 +48,46 @@ module Sourceninja
   end
 
   def self.send_package_info(package_data_hash)
-    Rails.logger.debug "Sourceninja: Attempting to send package information to SourceNinja"
+    if defined? Rails
+      Rails.logger.debug "Sourceninja: Attempting to send package information to SourceNinja"
+    end
 
     base_uri = @@base_uri
 
     if not ENV['SOURCENINJA_UPLOAD_URL'].nil? and ENV['SOURCENINJA_UPLOAD_URL'] != ""
-      Rails.logger.debug "Sourceninja: using #{ENV['SOURCENINJA_UPLOAD_URL']} for the upload URI"
+      if defined? Rails
+        Rails.logger.debug "Sourceninja: using #{ENV['SOURCENINJA_UPLOAD_URL']} for the upload URI"
+      end
+
       base_uri = ENV['SOURCENINJA_UPLOAD_URL']
     end
 
     if ENV['SOURCENINJA_TOKEN'].nil? or ENV['SOURCENINJA_TOKEN'] == ""
-      Rails.logger.debug "Sourceninja: No SOURCENINJA_TOKEN set, not uploading information to SourceNinja"
+      if defined? Rails
+        Rails.logger.debug "Sourceninja: No SOURCENINJA_TOKEN set, not uploading information to SourceNinja"
+      end
+
       return
     end
 
     if ENV['SOURCENINJA_PRODUCT_ID'].nil? or ENV['SOURCENINJA_PRODUCT_ID'] == ""
-      Rails.logger.debug "Sourceninja: No SOURCENINJA_PRODUCT_ID set, not uploading information to SourceNinja"
+      if defined? Rails
+        Rails.logger.debug "Sourceninja: No SOURCENINJA_PRODUCT_ID set, not uploading information to SourceNinja"
+      end
+
       return
     end
 
     params = { :id => ENV['SOURCENINJA_PRODUCT_ID'], :token => ENV['SOURCENINJA_TOKEN'], :package_info => { :package_details => package_data_hash }.to_json }
-    Rails.logger.debug "Sourceninja: Attempting to send package_info of #{params.to_s} to #{[base_uri,'rubygems/1_0'].join('/')}"
+
+    if defined? Rails
+      Rails.logger.debug "Sourceninja: Attempting to send package_info of #{params.to_s} to #{[base_uri,'rubygems/1_0'].join('/')}"
+    end
+
     response = HTTParty.post([base_uri,'rubygems/1_0'].join('/'), :body => params )
-    Rails.logger.debug "Sourceninja: Got back status #{response.code}"
+
+    if defined? Rails
+      Rails.logger.debug "Sourceninja: Got back status #{response.code}"
+    end
   end
 end
